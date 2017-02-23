0fd16e25614195b88044ac238c08fc14fe83b8bc
3vilguy <3vilguy@gmail.com>
2017-02-23 16:48:24 +0000

    Broadcast messages that we're getting from client to all connected clients.

--- 01-Neko-React/src/server/Server.hx
+++ 01-Neko-React/src/server/Server.hx
@@ -10,6 +10,8 @@ class Server
 	private static var HOST : String = "127.0.0.1";
 	private static var PORT : Int = 1234;
 
+	private var _serverLoop:WebSocketServerLoop<ClientData>;
+
 	static function main()
 	{
 		new Server();
@@ -17,13 +19,22 @@ class Server
 
 	public function new() 
 	{
-		var serverLoop:WebSocketServerLoop<ClientData> = new WebSocketServerLoop<ClientData>( function(socket:Socket) return new ClientData(socket) );
-		serverLoop.processIncomingMessage = handleIncomingMessage;
-		serverLoop.run(new Host(HOST), PORT);
+		_serverLoop = new WebSocketServerLoop<ClientData>( function(socket:Socket) return new ClientData(socket) );
+		_serverLoop.processIncomingMessage = handleIncomingMessage;
+		_serverLoop.run(new Host(HOST), PORT);
 	}
 
 	private function handleIncomingMessage( connection : ClientData, message : String )
 	{
 		trace("Incoming: " + message);
+		broadcastMessage(message);
+	}
+	
+	private function broadcastMessage( message : String ) 
+	{
+		for (client in _serverLoop.clients)
+		{
+			client.ws.send(message);
+		}
 	}
 }
