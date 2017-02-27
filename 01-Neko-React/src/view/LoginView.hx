52bb3230be8deae488f07a849b501a79454db9a1
3vilguy <3vilguy@gmail.com>
2017-02-27 00:52:30 +0000

    Add LoginView in React client part, so we can put server's address.

---
+++ 01-Neko-React/src/view/LoginView.hx
@@ -0,0 +1,47 @@
+package view;
+
+import react.ReactComponent;
+import react.ReactMacro.jsx;
+
+class LoginView extends ReactComponentOfProps<LoginViewProps>
+{
+	private static var HOST_DEFAULT : String = "127.0.0.1";
+
+
+	public function new(props:LoginViewProps) 
+	{
+		super(props);
+		
+		state = { btnEnabled: true };
+	}
+
+	override public function render():ReactElement 
+	{
+		return jsx('
+			< div style = {{height: "100vh", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}} >
+				<div style={{marginBottom: 10}}>
+					<div>
+						Server IP:
+					</div>
+					<div>
+						<input ref="address" defaultValue=$HOST_DEFAULT />
+					</div>
+				</div>
+
+				<div>
+					<button disabled=${!state.btnEnabled} onClick=$handleOnClick><b>Connect!</b></button>
+				</div>
+			</div>
+		');
+	}
+
+	private function handleOnClick()
+	{
+		setState({ btnEnabled: false });
+		props.connectHandler(refs.address.value);
+	}
+}
+
+typedef LoginViewProps = {
+	?connectHandler : String -> Void
+}
--- 01-Neko-React/src/view/MainView.hx
+++ 01-Neko-React/src/view/MainView.hx
@@ -6,43 +6,53 @@ import react.ReactMacro.jsx;
 
 class MainView extends ReactComponentOfState<MainViewState>
 {
-	private static var HOST_DEFAULT : String = "127.0.0.1";
 	private static var PORT : Int = 1234;
 
 	private var _ws : WebSocket;
+
 
 	public function new(props:Dynamic) 
 	{
 		super(props);
 		
-		state = { messages: [] };
-	}
-
-	override public function componentDidMount():Void 
-	{
-		setupSocketConnection();
+		state = {
+			isConnected: false,
+			messages: []
+		};
 	}
 
 	override public function render():ReactElement 
 	{
-		return jsx('
-			<div style={{height: "100vh", display: "flex", flexDirection: "column", justifyContent: "flex-end", backgroundColor: "#DDDDDD"}}>
-				<$ChatView messages=${state.messages} />
-				<div style = {{padding: 5, display: "flex"}}>
-					<input ref="input" placeholder="Type text here" onKeyPress=$onKeyPress style={{flexGrow: 2}} />
-					<button onClick=$sendMessage>Send</button>
-				</div>
-			<div/>
-		');
+		if (state.isConnected == false)
+		{
+			// Show login component
+			return jsx('
+				<$LoginView connectHandler=${connectToServer} />
+			');
+		}
+		else
+		{
+			// Show chat component
+			return jsx('
+				<div style={{height: "100vh", display: "flex", flexDirection: "column", justifyContent: "flex-end", backgroundColor: "#DDDDDD"}}>
+					<$ChatView messages=${state.messages} />
+					<div style = {{padding: 5, display: "flex"}}>
+						<input ref="input" placeholder="Type text here" onKeyPress=$onKeyPress style={{flexGrow: 2}} />
+						<button onClick=$sendMessage>Send</button>
+					</div>
+				<div/>
+			');
+		}
 	}
 
 
-	private function setupSocketConnection():Void
+	private function connectToServer( host : String ):Void
 	{
-		_ws = new WebSocket('ws://$HOST_DEFAULT:$PORT');
+		_ws = new WebSocket('ws://$host:$PORT');
 		_ws.onopen = function()
 		{
 			trace("CONNECT");
+			setState({ isConnected: true });
 		};
 		_ws.onmessage = function(e)
 		{
@@ -79,5 +89,6 @@ class MainView extends ReactComponentOfState<MainViewState>
 }
 
 typedef MainViewState = {
-	?messages : Array<String>
+	?isConnected : Bool,
+	?messages : Array<String>,
 }
