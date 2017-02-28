9de5588f1f7f7908927bf0c8f2131945c6805a9c
3vilguy <3vilguy@gmail.com>
2017-02-28 02:09:45 +0000

    Adding sockets connection in MainView class to check if it’s actually working (It does). Sending Introduction message (hitting server from 01-Neko-React).

--- 02-Neko-ReactNative/src/view/MainView.hx
+++ 02-Neko-ReactNative/src/view/MainView.hx
@@ -1,15 +1,25 @@
 package view;
 
+import haxe.Json;
+import js.html.WebSocket;
 import react.ReactComponent;
 import react.ReactMacro.jsx;
 import react.native.api.*;
 import react.native.component.*;
+import shared.Message;
+import shared.MsgConsts;
 import view.LoginView;
 
-class MainView extends ReactComponent {
+class MainView extends ReactComponent
+{
+	private static var PORT : Int = 1234;
+	private var _ws : WebSocket;
+
 	static var styles = Main.styles;
-	
-	function new(props) {
+
+
+	public function new(props)
+	{
 		super(props);
 		state = {
 			name: "",
@@ -27,7 +37,39 @@ class MainView extends ReactComponent {
 
 	private function connectToServer( host : String, name : String ):Void
 	{
-		trace('connectToServer');
-		trace(host, name);
+		_ws = new WebSocket('ws://$host:$PORT');
+		_ws.onopen = function()
+		{
+			trace("CONNECT");
+			setState({
+				isConnected: true,
+				name: name
+			});
+			sayHi(name);
+		};
+		_ws.onmessage = function(e)
+		{
+			trace("RECEIVE: " + e.data);
+			var messages:Array<String> = state.messages;
+			messages.push(e.data);
+			setState({ messages: messages });
+		};
+		_ws.onclose = function()
+		{
+			trace("DISCONNECT");
+		};
+	}
+
+	private function sayHi(name : String):Void
+	{
+		var introMsg:Message = { type: MsgConsts.INTRODUCTION_MSG, name: name };
+		sendMessage( Json.stringify(introMsg) );
+	}
+
+
+	private function sendMessage( msg : String ):Void
+	{
+		trace('Sending => $msg');
+		_ws.send(msg);
 	}
 }
